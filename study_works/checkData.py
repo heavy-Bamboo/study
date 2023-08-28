45eb7a5f83b5ab6ba246b31211e52ae1c2e4ef66
heavy-Bamboo <77606243+heavy-Bamboo@users.noreply.github.com>
2023-08-28 16:17:04 +0800

    Update checkData.py

--- study_works/checkData.py
+++ study_works/checkData.py
@@ -6,9 +6,9 @@ def mergefile(filepath,mergefile):
     iris_concat = pd.DataFrame()
     file = filepath
     iris1 = pd.ExcelFile(file)  # 读取Excel文件
-    iris1Df = iris1.parse('172.16.X.X')
-    iris2Df = iris1.parse('172.17.X.X')
-    iris3Df = iris1.parse('(灾备）172.18.X.X')
+    iris1Df = iris1.parse('172.X.X.X')
+    iris2Df = iris1.parse('172.X.X.X')
+    iris3Df = iris1.parse('(灾备）172.X.X.X')
     frames = [iris1Df,iris2Df,iris3Df]
     iris_concat = pd.concat(frames)#文件拼接
     iris_concat.to_excel(mergefile)  # 数据保存路径
@@ -36,7 +36,7 @@ def comparefile(filename,mergefilename):
                     continue
                 else:
                     j = j+1
-            elif "宁桥路灾备" in file:
+            elif "灾备" in file:
                 if origin1.iloc[k,2] == searchfile1.iloc[j,7] and searchfile1.iloc[j,7]!=None:
                     origin1.iloc[k,3] = searchfile1.iloc[j,6]
                     continue
@@ -47,7 +47,7 @@ def comparefile(filename,mergefilename):
 
 if __name__ == '__main__':
     if os.path.isfile('E:/python学习/存活IP合并.xlsx'):
-        comparefile('E:/python学习/宁桥路灾备.xlsx', 'E:/python学习/存活IP合并.xlsx')
+        comparefile('E:/python学习/灾备.xlsx', 'E:/python学习/存活IP合并.xlsx')
     else:
         mergefile('E:/python学习/存活IP-20201027.xlsx','E:/python学习/存活IP合并.xlsx')
-        comparefile('E:/python学习/宁桥路灾备.xlsx','E:/python学习/存活IP合并.xlsx')
+        comparefile('E:/python学习/灾备.xlsx','E:/python学习/存活IP合并.xlsx')
